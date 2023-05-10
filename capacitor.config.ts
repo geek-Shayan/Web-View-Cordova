8213a4c2ba5d6b3d3fa9b36f5927c8143eebb96c
geek-Shayan <shayanulhaqsadi@gmail.com>
2023-05-10 17:15:42 +0600

    done in app browser

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'com.sadi.webViewC',
   appName: 'web-view-c',
   webDir: 'www',
   server: {
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { InAppBrowser } from '@awesome-cordova-plugins/in-app-browser/ngx';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +8,31 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  constructor(private iab: InAppBrowser) {
+    // this.openBrowser()
+  }
+
+  openBrowser() {
+    
+    const browser = this.iab.create(
+      'https://sdk-microsite.tsports.com/live/0/+8801744555555/gp/WWpOQk9Wb3pRVzFrVkRCeVQwUm5kMDFVWXpCT1JGVXhUbFJWTVU1VFdteGpSREI1VFVSSmVrMUVXWGRPUkVVelRsUnJNVTlSUFQwPQ==',
+      // 'https://tsports.com/',
+      // 'https://toffeelive.com/',
+      // 'https://toffeelive.com/#video/07983a0e0b03d2c989d05f5d907ee2b3',
+      '_blank', //_blank _self
+      {
+        location: 'no',
+        hidden: 'no',
+        toolbar: 'yes',
+        toolbarposition: 'top',
+        mediaPlaybackRequiresUserAction: 'yes',
+        allowInlineMediaPlayback: 'yes',
+      
+      }
+      );
+  }
+
+  
+
 
 }
